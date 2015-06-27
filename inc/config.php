92ffbabff4eb780d9ed9d7cb0df7bc86efae302d
Allen Garvey <allengarvey@hotmail.com>
2015-06-27 00:21:10 -0400

    initial commit of php base project

---
+++ inc/config.php
@@ -0,0 +1,18 @@
+<?php 
+define('ENVIRONMENT_DEVELOPMENT', 0);
+define('ENVIRONMENT_PRODUCTION', 1);
+define('ENVIRONMENT_CURRENT', ENVIRONMENT_DEVELOPMENT);
+
+if(ENVIRONMENT_CURRENT === ENVIRONMENT_DEVELOPMENT){
+	define('BASE_URL','/folder_name/');
+	define('ROOT_PATH',$_SERVER['DOCUMENT_ROOT'] . '/folder_name/');
+}
+
+define('INC_PATH', ROOT_PATH.'inc/');
+define('VIEWS_PATH', INC_PATH.'views/');
+define('CONTROLLERS_PATH', INC_PATH.'controllers/');
+define('MODELS_PATH', INC_PATH.'models/');
+
+define('STYLES_URL', BASE_URL.'styles/');
+define('SCRIPTS_URL', BASE_URL.'scripts/');
+
