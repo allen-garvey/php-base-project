0d6efaa04da241b328415e51e74eec99167d70ca
Allen Garvey <allengarvey@hotmail.com>
2015-08-02 00:59:35 -0400

    update name for master.css file

--- inc/views/homepage.php
+++ inc/views/homepage.php
@@ -5,7 +5,7 @@
         <title></title>
         <meta name="description" content=""/>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
-        <link rel="stylesheet" type="text/css" href="<?= STYLES_URL.'style.css'; ?>"/>
+        <link rel="stylesheet" type="text/css" href="<?= STYLES_URL.'master.css'; ?>"/>
         <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
     </head>
     <body>
