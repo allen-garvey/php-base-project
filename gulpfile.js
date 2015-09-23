61018eeebc866d0ff69dc07ed796d2731dd078db
Allen Garvey <allengarvey@hotmail.com>
2015-09-23 00:45:26 -0400

    add constants to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -6,19 +6,23 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var maps = require('gulp-sourcemaps');
 
+var JS_SOURCE_DIR = 'js/';
+var JS_DEST_DIR = 'scripts/';
+var DIST_NAME = 'app'; //name of compiled file to be served i.e. app.js and app.min.js
+
 gulp.task('concatScripts', function(){
-	return gulp.src(['js/app.js'])
+	return gulp.src(['app'].map(function(file){return JS_SOURCE_DIR + file + '.js';}))
 		.pipe(maps.init())
-		.pipe(concat('app.js'))
+		.pipe(concat(DIST_NAME + '.js'))
 		.pipe(maps.write('./'))
-		.pipe(gulp.dest('scripts'));
+		.pipe(gulp.dest(JS_DEST_DIR));
 });
 
 gulp.task('minifyScripts', ['concatScripts'], function(){
-	return gulp.src('scripts/app.js')
+	return gulp.src(JS_DEST_DIR + DIST_NAME + '.js')
 		.pipe(uglify())
-		.pipe(rename('app.min.js'))
-		.pipe(gulp.dest('scripts'));
+		.pipe(rename(DIST_NAME + '.min.js'))
+		.pipe(gulp.dest(JS_DEST_DIR));
 });
 gulp.task('watchScripts', function(){
 	gulp.watch('js/**/*.js', ['minifyScripts']);
