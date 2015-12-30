5e034550753809a1765bfb72524ef09817e09dca
Allen Garvey <allengarvey@hotmail.com>
2015-12-29 22:25:46 -0500

    refactor to not need to set current env to set root path

--- inc/config.php
+++ inc/config.php
@@ -1,17 +1,19 @@
 <?php 
 define('ENVIRONMENT_DEVELOPMENT', 0);
 define('ENVIRONMENT_PRODUCTION', 1);
-require_once('current_environment.php'); //gets environment_current constant
 
-if(ENVIRONMENT_CURRENT === ENVIRONMENT_DEVELOPMENT){
-	define('BASE_URL','/folder_name/');
-	define('ROOT_PATH',$_SERVER['DOCUMENT_ROOT'] . '/folder_name/');
-}
-
+//paths
+define('ROOT_PATH', dirname(__FILE__, 2).'/');
 define('INC_PATH', ROOT_PATH.'inc/');
 define('VIEWS_PATH', INC_PATH.'views/');
 define('CONTROLLERS_PATH', INC_PATH.'controllers/');
 define('MODELS_PATH', INC_PATH.'models/');
+
+require_once(INC_PATH.'current_environment.php'); //gets environment_current constant
+
+if(ENVIRONMENT_CURRENT === ENVIRONMENT_DEVELOPMENT){
+	define('BASE_URL','/folder_name/');
+}
 
 define('STYLES_URL', BASE_URL.'styles/');
 define('SCRIPTS_URL', BASE_URL.'scripts/');
